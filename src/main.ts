b9f15260003fe5f3301930401df321c8867c99bf
Meno Abels <meno.abels@adviser.com>
2018-03-08 16:51:13 +0100

    * main

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import {
 } from '../src/index';
 import * as yargs from 'yargs';
 
-const main = yargs.usage('Usage: $0 <command> [options]')
+// tslint:disable-next-line:no-unused-expression
+yargs.usage('Usage: $0 <command> [options]')
   .command('watchComposer', 'start the watching Compose Server', {}, (argv) => {
     const cwd = PackageJson.findPathTo(process.cwd());
     console.log(`start the watching Compose Server in [${cwd}]`);
