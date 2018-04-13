9a83f08a473e07d5d386d9af767dc4b03b88f82e
janno <janno.rothfos@sinnerschrader.com>
2018-04-13 09:35:38 +0200

    introduced CreatePkgOptions to reduce parameters for createPkg()

--- src/functions/create-pkg.ts
+++ src/functions/create-pkg.ts
@@ -7,31 +7,35 @@ import { invokePackage } from './invoke-package';
 import { transformToCompose } from './transform-to-compose';
 import * as uuid from 'uuid';
 
-type objMap = { [id: string]: string };
+type ObjMap = { [id: string]: string };
+export interface CreatePkgOptions {
+  replace: boolean;
+  localDependencies: { [id: string]: string };
+}
 
-function addProjectPackages(pkgDeps: objMap, projectDeps: objMap, targetFolder: string): void {
-  if (!projectDeps) {
+function addProjectPackages(pkgDeps: ObjMap, targetFolder: string, options: CreatePkgOptions): void {
+  if (!options.localDependencies) {
     return;
   }
 
-  Object.keys(projectDeps).forEach((dependencyName) => {
+  Object.keys(options.localDependencies).forEach((dependencyName) => {
     if (pkgDeps[dependencyName]) {
-      const pathToProject = projectDeps[dependencyName];
+      const pathToProject = options.localDependencies[dependencyName];
       const packageJson = PackageJson.read(pathToProject);
-      if (pack(packageJson.name, pathToProject, targetFolder)) {
-        addProjectPackages(packageJson.dependencies, projectDeps, targetFolder);
+      if (pack(packageJson.name, pathToProject, targetFolder, options)) {
+        addProjectPackages(packageJson.dependencies, targetFolder, options);
       }
     }
   });
 }
 
-function pack(packageName: string, sourceFolder: string, targetFolder: string): boolean {
+function pack(packageName: string, sourceFolder: string, targetFolder: string, options: CreatePkgOptions): boolean {
   const composeDir = path.join(targetFolder, 'compose');
   const tmpDir = path.join(targetFolder, '.tmp', uuid.v4());
   const pkgFileName = path.join(composeDir, `${packageName}`);
   const tmpFileName = path.join(tmpDir, `${packageName}`);
 
-  if (fs.existsSync(`${pkgFileName}.npm.tgz`)) {
+  if (!options.replace && fs.existsSync(`${pkgFileName}.npm.tgz`)) {
     console.log(`${pkgFileName}.npm.tgz already exists. Skip packing...`);
     return false;
   }
@@ -47,23 +51,25 @@ function pack(packageName: string, sourceFolder: string, targetFolder: string): 
   return true;
 }
 
-export function createPkg(basePath: string = './', projectDependencies?: { [id: string]: string }): void {
+export function createPkg(basePath: string = './', options?: CreatePkgOptions): void {
   const packageJson = PackageJson.read(basePath);
   if (!PackageJson.isComposable(packageJson)) {
     return;
   }
 
+  options = options || { replace: false, localDependencies: {} };
+
   const appComposer = packageJson['app-composer'];
   const perCompose = transformToCompose(packageJson, appComposer);
 
-  const composePaths: objMap = {};
+  const composePaths: ObjMap = {};
   perCompose.forEach((entryPoints, composePath) => {
     composePath = path.resolve(basePath, composePath);
     const composeDir = path.join(composePath, 'compose');
     const pkgFname = path.join(composeDir, `${packageJson.name}`);
 
-    pack(packageJson.name, basePath, composePath);
-    addProjectPackages(packageJson.dependencies, projectDependencies, composePath);
+    pack(packageJson.name, basePath, composePath, options);
+    addProjectPackages(packageJson.dependencies, composePath, options);
 
     composePaths[composePath] = composePath;
 
